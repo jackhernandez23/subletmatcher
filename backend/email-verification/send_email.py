52d05c7ebdd7680a0e1ff45910cb91afe89e1a33
Aelly-A <aellyalwardi@gmail.com>
2024-11-07 15:35:11 -0500

    Create function to send email code

--- backend/email-verification/send_email.py
+++ backend/email-verification/send_email.py
@@ -1,38 +1,34 @@
+import smtplib
 from email.mime.text import MIMEText
-from pyotp import HOTP
 from smtplib import SMTP_SSL
+from pyotp import HOTP
 
 
-def send_email(subject, body, sender, recipients, password):
-    msg = MIMEText(body)
+def get_code(key_word):
+    key = "".join([letter for letter in key_word if letter.isalpha()])
+    hotp = HOTP(key)
+    code = hotp.at(0)
+
+    return code
+
+
+def send_email(recipient):
+    code = get_code(recipient)
+    subject = f"Subletter 2FA Code: {code}"
+    sender = "aelly.bot@gmail.com"  # This email is set up to send automated messages, it can be used in the product
+    message = f"Your code is {code}"
+    passphrase = "MY ACCOUNT PASSWORD"
+    msg = MIMEText(message)
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = recipients
-    with SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, recipients, msg.as_string())
-    print("Message sent!")
+    msg['To'] = recipient
 
+    try:
+        with SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+            smtp_server.login(sender, passphrase)
+            smtp_server.sendmail(sender, recipient, msg.as_string())
+            return True
+    except smtplib.SMTPAuthenticationError:
+        print("Check password")
 
-# Set up the key generator
-key = "base32secret3232"
-hotp = HOTP(key)
-counter = 0
-
-# Generate the key
-new_code = hotp.at(counter)
-
-# This would have to be stored in some other file, but counter increments by 1 after each time to generate new code
-counter += 1
-
-# The actual email contents
-message = f"Your code is {new_code}"
-me = "aelly.bot@gmail.com" # I set up this email to send automated messages, we can use this in the final product
-you = "aelly.alwardi@ufl.edu" # Test recepient
-sub = "Subletter 2FA Code"
-
-passphrase = "iynb jhrh ghro wjvw"
-
-
-
-send_email(sub, message, me, you, passphrase)
+    return False
